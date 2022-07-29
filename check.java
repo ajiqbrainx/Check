2644ff23de06c29a5a48e4c0c517283e5fe84b52
ajiqbrainx <94741631+ajiqbrainx@users.noreply.github.com>
2022-07-29 10:18:26 +0530

    Add files

---
+++ check.java
@@ -0,0 +1,11 @@
+public class Solution {
+
+    public static void main(String[] args) {
+
+
+
+        System.out.println("String: " );
+        System.out.println("Double: " );
+        System.out.println("Int: ");
+    }
+}
